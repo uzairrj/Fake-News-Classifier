695c0ddce9ea7be83b70636f88eb22f3f81ebfc3
uzairrj <uzairrj@gmail.com>
2022-06-20 00:45:10 +0200

    model reading

--- NLP/models.py
+++ NLP/models.py
@@ -2,4 +2,14 @@ import pickle
 
 class fake_news_models:
     __models = None
-    
+
+    def __init__(self, filename):
+        with open(filename, "rb") as fp:
+            __models = pickle.load(fp)
+            print("Models are loaded successfuly: ",__models.keys())
+
+    def get_model(self,key):
+        return self.__models[key]
+
+    def get_accuracy(self,key):
+        pass
